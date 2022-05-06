6a598dac0aee4b25651400e1e8ef6087b639e619
grantkee <grant@ether.ai>
2022-05-06 15:58:28 -0500

    conditions.sol - conditions, loops, public, and view functions

---
+++ conditions.sol
@@ -0,0 +1,47 @@
+// define the compiler version
+pragma solidity ^0.8.10;
+
+// start by creating a contract named Variables
+contract Conditions {
+    // state variable to store a number
+    uint8 public num;
+
+    // public function - can be called from within 
+    // the contract or externally
+
+    function set(uint _num) public {
+        num = _num;
+    }
+
+    // view function - doesn't change the state of
+    // any variable and does not require gas.
+    function get() public view returns (uint) {
+        return num;
+    }
+
+    // if/else in solidity
+    function foo(uint x) public return (uint) {
+        if (x < 10) {
+            return 0;
+        } else if (x < 20) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    // loop
+    function loop() public {
+        for (uint i = 0; i < 10; i++) {
+            if (i == 3) {
+                // skip to next iteration
+                continue;
+            }
+            if (i == 5) {
+                // exit loop
+                break;
+            }
+        }
+    }
+
+}
